3798c0deba8b8fa6d62d45d97d5d0159a92fa9ce
Jabaay <119708810+Jabaay@users.noreply.github.com>
2023-11-01 11:36:23 -0400

    Add files via upload

--- FOVSetting.cs
+++ FOVSetting.cs
@@ -10,9 +10,25 @@ public class FOVSetting : MonoBehaviour
     // instances of FOV
     private float radius;
     private float angle;
-
     private float newX;
     private float newY;
+
+    private PolygonCollider2D pc;
+    private LineRenderer lr;
+
+
+    // Start is called before the first frame
+    private void Start()
+    {
+        pc = GetComponent<PolygonCollider2D>();
+        lr = GetComponent<LineRenderer>();
+
+        lr.loop = true;
+        lr.startWidth = 0.15f; // start width
+        lr.endWidth = 0.15f; // end width
+        // lr.startColor = new Color(144, 238, 144); // start color
+        // lr.endColor = new Color(144, 238, 144); // end color
+    }
 
 
     // Update is called on every frame
@@ -27,25 +43,48 @@ public class FOVSetting : MonoBehaviour
         newY = radius * Mathf.Abs(Mathf.Sin(angle));
 
         setColliderPoints(); // call setColliderPoints()
+        updateLineRenderer(); // update lr
     }
 
 
     // Configure collider's shape with points
     private void setColliderPoints()
+    {
+        // Get the PolygonCollider2D component attached to your GameObject
+        PolygonCollider2D polygonCollider = gameObject.GetComponent<PolygonCollider2D>();
+
+        Vector2[] pathPoints = new Vector2[]
         {
-            // Get the PolygonCollider2D component attached to your GameObject
-            PolygonCollider2D polygonCollider = gameObject.GetComponent<PolygonCollider2D>();
+            new Vector2(newX, newY), // first point
+            new Vector2(newX, -newY), // second point
+            new Vector2(0, 0), // third point, fixed
+        };
 
-            Vector2[] pathPoints = new Vector2[]
+        // Update the path in the Polygon Collider
+        polygonCollider.SetPath(0, pathPoints);
+    }
+
+
+    // Update line renderer to match with polygon collider
+    private void updateLineRenderer()
+    {
+        if (lr)
+        {
+            // Get the points 
+            Vector2[] colliderPoints = pc.GetPath(0);
+            Vector3[] linePositions = new Vector3[colliderPoints.Length];
+
+            for (int i = 0; i < colliderPoints.Length; i++)
             {
-                new Vector2(newX, newY), // first point
-                new Vector2(newX, -newY), // second point
-                new Vector2(0, 0), // third point, fixed
-            };
+                Vector3 worldPoint = transform.TransformPoint(colliderPoints[i]);
+                linePositions[i] = new Vector3(worldPoint.x + 6.55f, worldPoint.y, 0f);
+            }
 
-            // Update the path in the Polygon Collider
-            polygonCollider.SetPath(0, pathPoints);
+            // Set line renderer positions
+            lr.positionCount = linePositions.Length;
+            lr.SetPositions(linePositions);
         }
+    }
 
 }
 
