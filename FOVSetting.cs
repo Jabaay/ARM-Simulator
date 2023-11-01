ca6b666b980a86ffe2db52fa33822ccb51c25453
Jabaay <119708810+Jabaay@users.noreply.github.com>
2023-10-31 22:35:46 -0400

    Add files via upload
    2D Polygon Collider (FOV) Customization

---
+++ FOVSetting.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVSetting : MonoBehaviour
+{
+    // script communication
+    public FOVSlider fovSlider; 
+
+    // instances of FOV
+    protected float radius;
+    protected float angle;
+
+    private float newX;
+    private float newY;
+
+
+    // Update is called on every frame
+    void Update()
+    {
+        // Get the values from the corresponding sliders
+        radius = fovSlider._radiusSlider.value;
+        angle = fovSlider._angleSlider.value * Mathf.PI / 180; // degree to radian conversion
+
+        // Compute x and y from radius and angle
+        newX = radius * Mathf.Abs(Mathf.Cos(angle));
+        newY = radius * Mathf.Abs(Mathf.Sin(angle));
+
+        setColliderPoints(); // call setColliderPoints()
+    }
+
+
+    // Configure collider's shape with points
+    private void setColliderPoints()
+        {
+            // Get the PolygonCollider2D component attached to your GameObject
+            PolygonCollider2D polygonCollider = gameObject.GetComponent<PolygonCollider2D>();
+
+            // Define the points for the collider
+            int pathIndex = 0;
+
+            Vector2[] pathPoints = new Vector2[]
+            {
+                new Vector2(newX, newY), // first point
+                new Vector2(newX, -newY), // second point
+                new Vector2(0, 0), // third point, fixed
+            };
+
+            // Update the path in the Polygon Collider
+            polygonCollider.SetPath(pathIndex, pathPoints);
+        }
+
+}
+
+
---
+++ FOVSlider.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FOVSlider : MonoBehaviour
+{
+    // Slider and Text Objects
+    [SerializeField] public Slider _radiusSlider;
+    [SerializeField] public Slider _angleSlider;
+
+    [SerializeField] private Text _radiusSliderVal;
+    [SerializeField] private Text _angleSliderVal;
+
+    protected float radius;
+    protected float angle;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        // add listeners to both sliders to update the texts
+        _radiusSlider.onValueChanged.AddListener(OnSlider1ValueChanged);
+        _angleSlider.onValueChanged.AddListener(OnSlider2ValueChanged);
+    }
+
+
+    // Change the value of radius slider
+    private void OnSlider1ValueChanged(float value)
+    {
+        // update the text of the radius slider
+        _radiusSliderVal.text = value.ToString("0.00");
+    }
+
+
+    // Change the value of angle slider
+    private void OnSlider2ValueChanged(float value)
+    {
+        // update the text of the angle slider
+        _angleSliderVal.text = value.ToString("0.00");
+    }
+
+}
