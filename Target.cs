6f8968ca280adcd68795ef1d105f4bf955af309b
Jabaay <119708810+Jabaay@users.noreply.github.com>
2023-11-15 03:11:12 -0500

    Update Target.cs

--- Target.cs
+++ Target.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour
+public class Target: MonoBehaviour
 {
 
     // Start is called before the first frame update
@@ -12,14 +12,14 @@ public class Target : MonoBehaviour
         // Get Jammer position
         Vector3 jammerPos = GameObject.Find("Jammer").transform.position;
 
-        // Target spawn postition
+        // Target spawn position
         // Make sure it won't collide with the Jammer
         Vector3[] targetSpawnPos = new Vector3[]
         {
-            new Vector3(Random.Range(20f, jammerPos.x - 8f), Random.Range(-72.6f, jammerPos.y - 8f), 0), // left side under the jammer
-            new Vector3(Random.Range(jammerPos.x + 8f, 242.4f), Random.Range(-72.6f, jammerPos.y - 8f), 0), // right side under the jammer
-            new Vector3(Random.Range(20f, jammerPos.x - 8f), Random.Range(jammerPos.y + 8f, 0), 0), // left side above the jammer
-            new Vector3(Random.Range(jammerPos.x + 8f, 242.4f), Random.Range(jammerPos.y + 8f, 0), 0) // right side above the jammer
+            new Vector3(Random.Range(20f, jammerPos.x - 15f), Random.Range(-72.6f, jammerPos.y - 15f), 0), // left side under the jammer
+            new Vector3(Random.Range(jammerPos.x + 12f, 242.4f), Random.Range(-72.6f, jammerPos.y - 15f), 0), // right side under the jammer
+            new Vector3(Random.Range(20f, jammerPos.x - 8f), Random.Range(jammerPos.y + 15f, 0), 0), // left side above the jammer
+            new Vector3(Random.Range(jammerPos.x + 15f, 242.4f), Random.Range(jammerPos.y + 15f, 0), 0) // right side above the jammer
         };
 
         transform.position = targetSpawnPos[Random.Range(0, targetSpawnPos.Length - 1)];
